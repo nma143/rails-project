46ca4664e7beeae15415d6752e4166a12a5f43bc
nma143 <nikki.apostolakis@gmail.com>
2019-03-03 15:41:54 -0500

    add validation to review model, build out new review view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,19 @@
 class ReviewsController < ApplicationController
 
   def new
-    
+    @review = Review.new
     @books_to_review = Book.all - current_user.books
+  end
+
+  def create
+    @review = Review.new(:content => params[:content], :stars => params[:stars], :user_id => session[:user_id], :book_id => params[:book_id])
+
+    if @review.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+
   end
 
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :content, presence: true
+
+
 end
