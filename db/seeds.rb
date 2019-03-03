50355028cf3bdbf456d37580a0edf4dabbb3e49a
nma143 <nikki.apostolakis@gmail.com>
2019-03-02 19:39:23 -0500

    add books to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Seed database with a library of books
+
+Book.create(title: "Harry Potter 1", author: "JKR")
+Book.create(title: "Harry Potter 2", author: "JKR")
+Book.create(title: "Lord of the Rings", author: "JRRT")
