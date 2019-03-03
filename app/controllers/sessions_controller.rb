8d3f7a9c6390e18c3c32c5c1421864552479a302
nma143 <nikki.apostolakis@gmail.com>
2019-03-03 12:07:51 -0500

    fix field names in sessions new view and add to sessions create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,14 @@ class SessionsController < ApplicationController
 
   def create
 
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
+
   end
 
 end
