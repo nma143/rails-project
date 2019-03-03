dfecc9cc6d7a06fb22f8482e1edf31308a015e43
nma143 <nikki.apostolakis@gmail.com>
2019-03-03 11:41:07 -0500

    add format validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,15 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
 
+  #username can only be lowercase letters and numbers
+  validates_format_of :username, :with => /\A[a-z0-9]+\z/
+
+  #email format - simplest successful format is a@b.c
+  validates_format_of :email, :with => /\A[a-z0-9\._]+@[a-z\d\-]+(\.[a-z\d\-]+)\z/
+
   validates :username, uniqueness:  true
   validates :email, uniqueness:  true
+
+
 
 end
