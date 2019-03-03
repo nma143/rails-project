4b1d5fa948ad2e93d1b878fe14da2930f826145d
nma143 <nikki.apostolakis@gmail.com>
2019-03-03 11:13:09 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   has_many :books, through: :reviews
 
   has_secure_password
+
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  validates :username, uniqueness:  true
+  validates :email, uniqueness:  true
+
 end
