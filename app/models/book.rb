93172de5171aae9628422802d858d5bd76bacd9e
nma143 <nikki.apostolakis@gmail.com>
2019-03-13 16:41:46 -0400

    create book instance method average_star_rating, add average_star_rating to index views for books and reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,14 @@
 class Book < ApplicationRecord
   has_many  :reviews
   has_many :users, through: :reviews
+
+
+  def average_star_rating
+
+    if self.reviews.size >=1
+      star_ratings = self.reviews.collect {|review| review.stars}
+      avg_star_rating = (star_ratings.sum/star_ratings.size.to_f).round(1)
+    end
+  end
+
 end
