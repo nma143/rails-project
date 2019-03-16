23e163c58142a47a5ffdd621d2b6f739ac039115
nma143 <nikki.apostolakis@gmail.com>
2019-03-16 15:58:11 -0400

    add stars validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  validates :stars, presence: true
   validates :content, presence: true
-
+  validates :stars, inclusion: { in: [0, 1, 2, 3]}
 
 end
