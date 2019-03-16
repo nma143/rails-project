e5a08e35b157f6d03021d4590edbf34bd4d78d91
nma143 <nikki.apostolakis@gmail.com>
2019-03-16 12:56:10 -0400

    remove show method from books controller and remove comments

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,10 @@ before_action :require_logged_in
     @books = Book.order_by_title
   end
 
-  def show
-
-  end
-
   def most_reviewed
 
     @books = Book.order_by_most_reviewed
     render :index
-    #Good! I want to show the number of reviews now
-    #Also, I think I could probably use a partial here to make this view
-    # a little different than the index view
 
   end
 
