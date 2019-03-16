eb61820663c981b20c2c04d3211ed28c88a14af2
nma143 <nikki.apostolakis@gmail.com>
2019-03-16 19:17:18 -0400

    add custom validation to prevent a user from submitting more than 1 review for a book

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,12 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :stars, inclusion: { in: [0, 1, 2, 3]}
 
+  validate  :user_has_not_reviewed_book_already
+
+  def user_has_not_reviewed_book_already
+    if Review.find_by(user_id: user_id, book_id: book_id)
+      errors.add(:book_id, "user already reviewed this book")
+    end
+  end
+
 end
