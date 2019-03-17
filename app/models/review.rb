2b59b356d01870ef2bc74146a3a337842901a2c7
nma143 <nikki.apostolakis@gmail.com>
2019-03-16 20:38:21 -0400

    change user_has_not_reviewed_book_already validation so it doesn't stop an update

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,7 +9,11 @@ class Review < ApplicationRecord
   validate  :user_has_not_reviewed_book_already
 
   def user_has_not_reviewed_book_already
-    if Review.find_by(user_id: user_id, book_id: book_id)
+    # If the review already has an id, then we are doing an update
+    # This validation should not stop an update.
+    # If the review has no id, it's a new review, so check to see if the
+    # user has already reviewed the book
+    if !id && Review.find_by(user_id: user_id, book_id: book_id)
       errors.add(:user_id, "already reviewed this book")
     end
   end
