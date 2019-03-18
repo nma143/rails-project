3e12d7afef6e1410d7879d6867aca21190bfc4d6
nma143 <nikki.apostolakis@gmail.com>
2019-03-18 13:40:23 -0400

    create User class methods for creating a valid username from an email, and edit the session_controller create method for facebook log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,15 +12,18 @@ class SessionsController < ApplicationController
       name = auth_hash[:extra][:raw_info][:name] #e.g. Jane Smith
 
       # If not first time this email has been used to log in, just log them in
-      if User.find_by(:email => email)
+      if user = User.find_by(:email => email)
         raise "You already exist"
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
 
       # If this is the first time this email has been used:
       # create a user with facebook email, change facebook user name to meet db requirements, and give fake password
       else
+        user = User.new
+        auto_username = user.create_username(name) 
+        fake_password = SecureRandom.hex
         raise "You don't exist"
-        
-
       end
 
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,37 @@ class User < ApplicationRecord
   validates :username, uniqueness:  true
   validates :email, uniqueness:  true
 
+  def self.create_username_from_email(email)
+    # used when user is created via a facebook login
+
+    # get a starting point for the username from the email
+    # if can't get something to work with from the email, just use anonymous
+    email_root = email.split("@")[0]
+    username_root = /[a-z0-9]+/.match(email_root)[0] if /[a-z0-9]+/.match(email_root)
+    username_root ||= "anonymous"
+
+    # By now username_root won't be blank, and won't contain invalid characters
+    # Will need to check it is unique
+    # If not, add a suffix that results in unquie username
+    suffix = 0
+    username = username_root
+    loop do
+      user = self.new(username: username)
+      user.validate_username
+      if user.errors[:username].include?("has already been taken")
+        suffix = suffix + 1
+        username = username_root + suffix.to_s
+      else
+        break
+      end
+    end
+    username
+  end
+
+    def validate_username
+      self.class.validators_on(:username).each do |validator|
+        validator.validate_each(self, :username, self.username)
+      end
+    end
+
 end
